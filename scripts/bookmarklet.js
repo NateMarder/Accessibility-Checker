ef35a0aaf52c9d1b66ae748829a900cfdfda58fc
Nate Marder <nmarder@navexglobal.com>
2017-06-25 17:49:54 -0700

    put urls back for now

--- scripts/bookmarklet.js
+++ scripts/bookmarklet.js
@@ -13,10 +13,10 @@ javascript: (
                 fileRef = document.createElement( 'link' );
                 fileRef.rel = 'stylesheet';
                 fileRef.type = 'text/css';
-                fileRef.href = 'file:///C:/NateProjects/AccessibilityProject/styles/tool.css';
+                fileRef.href = 'styles/tool.css';
                 document.getElementsByTagName( 'head' )[0].appendChild( fileRef );
                 fileRef = document.createElement( 'script' );
-                fileRef.src = 'file:///C:/NateProjects/AccessibilityProject/scripts/logic.js';
+                fileRef.src = 'scripts/logic.js';
                 fileRef.type = 'text/javascript';
                 document.getElementsByTagName( 'head' )[0].appendChild( fileRef );
             }
